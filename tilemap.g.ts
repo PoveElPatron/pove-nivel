6ab48e998f3799a782da220de7c087e76f12be48
PoveElPatron <124886549+PoveElPatron@users.noreply.github.com>
2023-02-09 12:58:19 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,71 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "nivel1":
+            case "nivel1":return tiles.createTilemap(hex`28002800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001f20202020202020202020202020202020202020202020202020202020202020202020202020201e221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d081d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d091d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d232323231c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21221c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d1c1d21071a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a06041b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b04031a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a05021b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b02021a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a020416171617161716171617161718191817181718171817181718171817181718171817181718170407131515151515151515151515151515151515151515151515151515151515151515151515151406071315151515151515151515151515151515151515151515151515151515151515151515151514060713151515151515151515151515151515151515151515151515151515151515151515151515140607131515151515151515151515151515151515151515151515151515151515151515151515151406071315151515151515151515151515151515151515151515151515151515151515151515151514060713151515151515151515151515151515151515151515151515151515151515151515151515140607131515151515151515151515151515151515151515151515151515151515151515151515151406071315151515151515151515151515151515151515151515151515151515151515151515151514060713151515151515151515151515151515151515151515151515151515151515151515151515140607131515151515151515151515151515151515151515151515151515151515151515151515151406070f0f0f0f0a0d0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f06070e0e0e0e0b0c0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e06070f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0a0d0f0f0f0f06070e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0b0c0e0e0e0e06040f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0a0d0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0411101010101010101010101010101010100b0c10101010101010101010101010101010101010101202101010101010101024242410101010101010101010101010101010101010101010101010101002020a0d0f0f0f240f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0a0d0f0f0f0f0f0f0f0f0f0f0f02040b0c0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0e0b0c0e0e0e0e0e0e0e0e0e0e0e0401020102010201020102010201020102010201020102010201020102010201020102010201020102`, img`
+........................................
+........................................
+........................................
+........................................
+.......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2......................................2
+2........222...........................2
+2.....2................................2
+2......................................2
+2222222222222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorLight1,sprites.dungeon.purpleOuterEast2,sprites.dungeon.floorLight3,sprites.dungeon.purpleOuterWest2,sprites.dungeon.stairWest,sprites.dungeon.stairEast,sprites.dungeon.doorOpenNorth,sprites.dungeon.doorOpenSouth,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.builtin.forestTiles5,sprites.builtin.forestTiles7,sprites.builtin.forestTiles24,sprites.builtin.crowd6,sprites.builtin.crowd7,sprites.builtin.crowd2,sprites.builtin.crowd3,sprites.dungeon.hazardHole,sprites.castle.rock2,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn1,sprites.vehicle.roadIntersection3,sprites.vehicle.roadIntersection4,sprites.vehicle.roadIntersection2,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorDark3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
